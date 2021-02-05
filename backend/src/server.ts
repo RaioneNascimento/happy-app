74f01f6e0ea6088fa95a161de22e5ba9f15f9605
Raione Nascimento <56979345+RaioneNascimento@users.noreply.github.com>
2021-02-05 00:26:53 -0300

    models, routes, uploadImages

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,28 +1,11 @@
 import express from 'express';
-
 import './database/connection';
+
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
-
-// Rota = conjunto
-// Recurso = usuário
-
-// Métodos HTTP = GET, POST, PUT, DELETE
-// Parâmetros
-
-// GET = Buscar uma informação (Lista, Item)
-// POST = Criando uma informação
-// PUT = Editando uma informação
-// DELETE = Deletando uma informação
-
-// QUERY Params: http://localhost:3333/users?search=raione
-// ROUTE Params: http://localhost:3333/users/1 (Identificar um recurso)
-// BODY: http://localhost:3333/users (Identificar um recurso)
-
-app.get('/users', (request, response) => {
-  return response.json({ message: 'Teste' });
-});
+app.use(routes);
 
 app.listen(3333);
